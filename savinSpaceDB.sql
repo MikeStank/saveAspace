bda7fbc60efeb994c8e8a712a3a70782acd70e60
Mike Stankiewicz <stankiem89@gmail.com>
2018-11-06 08:12:52 -0700

    added sql DB

---
+++ savinSpaceDB.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS savinSpace;
+CREATE DATABASE savinSpace;
+USE savinSpace;
+
+-- Create table
+CREATE TABLE users (
+id INT AUTO_INCREMENT,
+user_first_name VARCHAR(30) NOT NULL,
+user_last_name VARCHAR(30) NOT NULL,
+user_EMAIL VARCHAR(30) NOT NULL,
+user_PW VARCHAR(30) NOT NULL,
+PRIMARY KEY(id)
+);
+
+-- Inserted a set of records into the table
+INSERT INTO users (user_first_name, user_last_name, user_EMAIL, user_PW) 
+VALUES ("Jerry", "Seinfeld", "jseinfeld@gmail.com", "iLoveElaineB95"),
+("Crazy Joe", "Davola", "clownboy@yahoo.com", "candleLight1");
